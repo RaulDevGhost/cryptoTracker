511d59a7fce328913348185ae2a8e91452a3d01e
rri <raul.rincones@adorsys.com>
2023-02-17 16:44:15 +0100

    FACEID new package test

--- src/components/register/HomeRegisterScreen.js
+++ src/components/register/HomeRegisterScreen.js
@@ -1,46 +1,71 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
 import colors from '../../utils/colors';
 import {useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import ReactNativeBiometrics from 'react-native-biometrics';
+import TouchID from 'react-native-touch-id';
 
 const HomeRegisterScreen = () => {
-  const rnBiometrics = new ReactNativeBiometrics();
+  const optionalConfigObject = {
+    title: 'Authentication Required', // Android
+    imageColor: '#e00606', // Android
+    imageErrorColor: '#ff0000', // Android
+    sensorDescription: 'Touch sensor', // Android
+    sensorErrorDescription: 'Failed', // Android
+    cancelText: 'Cancel', // Android
+    fallbackLabel: 'Show Passcode', // iOS (if empty, then label is hidden)
+    unifiedErrors: false, // use unified error messages (default false)
+    passcodeFallback: false, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
+  };
+  const pressHandler = () => {
+    TouchID.authenticate(
+      'to demo this react-native component',
+      optionalConfigObject,
+    )
+      .then(success => {
+        alert('Authenticated Successfully');
+        console.log('success', success);
+      })
+      .catch(error => {
+        alert('Authentication Failed');
+      });
+  };
+  // const rnBiometrics = new ReactNativeBiometrics();
 
-  rnBiometrics.isSensorAvailable().then(resultObject => {
-    const {available, biometryType} = resultObject;
+  // rnBiometrics.isSensorAvailable().then(resultObject => {
+  //   const {available, biometryType} = resultObject;
 
-    if (available && biometryType === rnBiometrics.TouchID) {
-      console.log('TouchID is supported');
-    } else if (available && biometryType === rnBiometrics.FaceID) {
-      console.log('FaceID is supported');
-    } else if (available && biometryType === rnBiometrics.Biometrics) {
-      console.log('Biometrics is supported');
-      try {
-        rnBiometrics
-          .simplePrompt({
-            promptMessage: 'Confirm fingerprint',
-          })
-          .then(resultObject => {
-            const {success} = resultObject;
+  //   if (available && biometryType === rnBiometrics.TouchID) {
+  //     console.log('TouchID is supported');
+  //   } else if (available && biometryType === rnBiometrics.FaceID) {
+  //     console.log('FaceID is supported');
+  //   } else if (available && biometryType === rnBiometrics.Biometrics) {
+  //     console.log('Biometrics is supported');
+  //     try {
+  //       rnBiometrics
+  //         .simplePrompt({
+  //           promptMessage: 'Confirm fingerprint',
+  //         })
+  //         .then(resultObject => {
+  //           const {success} = resultObject;
 
-            if (success) {
-              console.log('successful biometrics provided');
-            } else {
-              console.log('user cancelled biometric prompt');
-            }
-          })
-          .catch(() => {
-            console.log('biometrics failed');
-          });
-      } catch (e) {
-        console.log('Device not Support Fingerprint');
-      }
-    } else {
-      console.log('Biometrics not supported');
-    }
-  });
+  //           if (success) {
+  //             console.log('successful biometrics provided');
+  //           } else {
+  //             console.log('user cancelled biometric prompt');
+  //           }
+  //         })
+  //         .catch(() => {
+  //           console.log('biometrics failed');
+  //         });
+  //     } catch (e) {
+  //       console.log('Device not Support Fingerprint');
+  //     }
+  //   } else {
+  //     console.log('Biometrics not supported');
+  //   }
+  // });
 
   const navigation = useNavigation();
 
@@ -70,6 +95,9 @@ const HomeRegisterScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}>Home Register2</Text>
+      <TouchableHighlight onPress={pressHandler}>
+        <Text>Authenticate with Touch ID</Text>
+      </TouchableHighlight>
     </View>
   );
 };
